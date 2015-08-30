27f975fd8406d003f9028fd3607160d030af64c8
TheLarkInn <sean.larkin@cuw.edu>
2015-08-30 12:19:17 -0500

    Added responsive navigation styles

--- app/components/SLNavBar/SLNavBar.js
+++ app/components/SLNavBar/SLNavBar.js
@@ -17,19 +17,18 @@ export default class SLNavBar extends React.Component {
 	render() {
 		let navItems = ['HOME', 'ABOUT', 'WORK', 'CONTACT'].map( (item, index) => {
 			let activeClass = '';
-
 			if (this.props.activeNavIndex == index) {
 				activeClass = 'sl-button__active';
 			}
-
 			return (
-				<a href='' key={index} onClick={this._navItemWasSelected.bind(this, index)} className={'sl-navbar--item sl-button_nav-button ' + activeClass}>
+				<a  href='' 
+                    key={index} 
+                    onClick={this._navItemWasSelected.bind(this, index)} 
+                    className={'sl-navbar--item sl-button_nav-button ' + activeClass}>
 					{item}
 				</a>
 			);
-
 		});
-
 
 		return (
 			<nav className='sl-navbar'>
